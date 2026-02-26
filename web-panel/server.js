06f76fc6fbc7cf8d6870fa38a14c70fff5b1f3ad
unknown <1SMRA-scamren559@IESMARTINEZM.ES>
2026-02-26 11:00:53 +0100

    Fix: Use path.join for static files directory

--- web-panel/server.js
+++ web-panel/server.js
@@ -35,7 +35,7 @@ try {
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const VM_PREFIX = 'ubuntu-vm-';
 const BASE_SSH_PORT = 2222;
